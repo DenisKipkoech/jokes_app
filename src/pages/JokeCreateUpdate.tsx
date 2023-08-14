c8fb83cf8f0fb46c9340b374f04ccebe36031838
Denis Rotich <denis@gror.io>
2023-08-14 10:29:54 +0300

    Fix: replaced tables and completed proejct

--- src/pages/JokeCreateUpdate.tsx
+++ src/pages/JokeCreateUpdate.tsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
-import { useQuery } from 'react-query';
-import { useLocation, useParams } from 'react-router-dom'
-import { baseGet } from '../services/api';
-import { Container, Typography } from '@mui/material';
-import JokesForm from '../sections/JokesForm';
-import Header from '../components/Header';
+import React, { useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { baseDelete, baseGet } from "../services/api";
+import { Button, Container, Stack, Typography } from "@mui/material";
+import JokesForm from "../sections/JokesForm";
+import Header from "../components/Header";
+import { LoadingButton } from "@mui/lab";
+import { Close, Delete } from "@mui/icons-material";
 
 type Joke = {
-    id?: number;
-    title?: string;
-    author?: string;
-    createdAt?: string;
-    views?: number;
+  id?: number;
+  title?: string;
+  author?: string;
+  createdAt?: string;
+  views?: number;
 };
 
 // type JokeInfo = {
@@ -22,54 +24,96 @@ type Joke = {
 //     Views?: number;
 // };
 
-
 export default function JokeCreateUpdate() {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-    const {id = ''} = useParams();
+  const navigate = useNavigate();
 
-    const [joke, setJoke] = useState<Joke>({
-        title:'',
-        author:'',
-        createdAt:'',
-        views:0,
+  const client = useQueryClient();
+
+  const { id = "" } = useParams();
+
+  const [joke, setJoke] = useState<Joke>({
+    title: "",
+    author: "",
+    createdAt: "",
+    views: 0,
+  });
+
+  const isEdit = pathname.includes("edit");
+
+  const fetchJoke = useQuery(["get_joke", id], () => baseGet(`/${id}`), {
+    refetchOnWindowFocus: false,
+    enabled: isEdit,
+    onSuccess: (data) => {
+      const info: Joke = data.data;
+      // console.log(info);
+      // setJoke({
+      //     id: info?.id,
+      //     title: info.Title,
+      //     author: info.Author,
+      //     createdAt: info.CreatedAt,
+      //     views: info.Views
+      // })
+      setJoke(info);
+    },
+    onError: () => alert("Failed to get joke!"),
+  });
+
+  const deleteJoke = useMutation((id?: number) => baseDelete(`${id}`));
+
+  const onClose = () => {
+    navigate("/");
+  };
+
+  const onDeleteJoke = (id?: number) => {
+    deleteJoke.mutate(id, {
+      onSuccess: () => {
+        alert("Joke deleted successfully!");
+        client.invalidateQueries("all_jokes");
+        navigate("/");
+      },
+      onError: () => {
+        alert("Failed to delete joke");
+      },
     });
-
-    const isEdit = pathname.includes('edit');
-
-    const fetchJoke = useQuery(
-        ['get_joke', id],
-        () => baseGet(`/${id}`),
-        {
-            refetchOnWindowFocus: false,
-            enabled: isEdit,
-            onSuccess: (data) => {
-                const info:Joke = data.data;
-                // console.log(info);
-                // setJoke({
-                //     id: info?.id,
-                //     title: info.Title,
-                //     author: info.Author,
-                //     createdAt: info.CreatedAt,
-                //     views: info.Views
-                // })
-                setJoke(info)
-            },
-            onError: () => alert('Failed to get joke!'),
-        }
-    )
-
+  };
 
   return (
-    <Container maxWidth="lg">
-        <br />
-        <Header />
-        <br />
+    <Container maxWidth="md">
+      <br />
+      <Header />
+      <br />
 
-        <Typography variant='h4' paragraph>{isEdit ? `Edit ${joke?.title}` : 'Add New Joke'}</Typography>
+      <Typography variant="h4" paragraph>
+        {isEdit ? `Edit ${joke?.title}` : "Add New Joke"}
+      </Typography>
 
-        <JokesForm isEdit={isEdit} joke={joke} loading={fetchJoke.isLoading}/>
+      {isEdit && (
+        <Stack direction="row" justifyContent={"space-between"}>
+          <Button
+            variant="outlined"
+            color="info"
+            onClick={onClose}
+            startIcon={<Close />}
+          >
+            close
+          </Button>
 
+          <LoadingButton
+            variant="outlined"
+            color="error"
+            onClick={() => onDeleteJoke(joke?.id)}
+            loading={deleteJoke.isLoading}
+            startIcon={<Delete />}
+          >
+            Delete
+          </LoadingButton>
+        </Stack>
+      )}
+      <br />
+
+      <JokesForm isEdit={isEdit} joke={joke} loading={fetchJoke.isLoading} />
     </Container>
-  )
+  );
 }
--- src/pages/JokesPage.tsx
+++ src/pages/JokesPage.tsx
@@ -1,18 +1,23 @@
+import React, { useState } from "react";
+
 import {
+  Button,
   Container,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
+  Link,
+  Stack,
   Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+
+import MUIDataTable from "mui-datatables";
+
 import { useQuery } from "react-query";
 import { baseGet } from "../services/api";
+
 import Header from "../components/Header";
+
+import moment from "moment";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { Add } from "@mui/icons-material";
 
 type Joke = {
   id?: number;
@@ -23,76 +28,212 @@ type Joke = {
 };
 
 export default function JokesPage() {
-  const [jokes, setJokes] = useState([]);
+  const navigate = useNavigate();
 
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [jokes, setJokes] = useState<Joke[]>([]);
+
 
   useQuery("all_jokes", () => baseGet(""), {
     refetchOnWindowFocus: false,
-    onSuccess: (data) => setJokes(data.data),
+    onSuccess: (data) => {
+      setJokes(data.data);
+    },
     onError: () => alert("Failed to get jokes!"),
   });
 
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
+  const onAdd = () => {
+    navigate("joke/new");
   };
 
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
+  const formatAuthor = (val?: string): string => {
+    if (val) {
+      // eslint-disable-next-line no-useless-escape
+      const regex = /@([^\.]+)\./g;
+      const replacement = "@***.";
+
+      const result = val.replace(regex, replacement);
+      return result;
+    } else {
+      return "-";
+    }
   };
+
+  const formatDate = (val?: string) => {
+    if (val) {
+      return moment(val).format("DD MMM YYYY");
+    } else {
+      return "-";
+    }
+  };
+
+  const getViewColor = (views?: number): string => {
+    if (views) {
+      if (views >= 0 && views <= 25) {
+        return "tomato";
+      } else if (views >= 26 && views <= 50) {
+        return "orange";
+      } else if (views >= 51 && views <= 75) {
+        return "yellow";
+      } else if (views >= 76 && views <= 100) {
+        return "green";
+      } else {
+        return "grey";
+      }
+    } else {
+      return "grey";
+    }
+  };
+
+  const columns = [
+    {
+     name: "title",
+     label: "Title",
+     options: {
+      filter: false,
+      sort: false,
+      customBodyRenderLite: (dataIndex:number) => (
+        <Link component={RouterLink} to={`/joke/${jokes[dataIndex].id}/edit`}>
+          {jokes[dataIndex].title|| "-"}
+        </Link>
+      )
+     }
+    },
+    {
+     name: "author",
+     label: "Author",
+     options: {
+      filter: false,
+      sort: false,
+      customBodyRender: (value: string) => { return formatAuthor(value)}
+     }
+    },
+    {
+     name: "createdAt",
+     label: "Created Date",
+     options: {
+      filter: true,
+      sort: true,
+      customBodyRender: (value: string) => { return formatDate(value)}
+     }
+    },
+    {
+     name: "views",
+     label: "Views",
+     options: {
+      filter: true,
+      sort: true,
+      customBodyRender: (value: number) => (
+        <span style={{color: getViewColor(value)}}>{value}</span>
+      )
+      
+     }
+    },
+  ];
+
+  const options = {
+    rowsPerPage: 5,
+    rowsPerPageOptions:[5,10]
+  }
 
   return (
     <>
-      <Container maxWidth="xl">
+      <Container maxWidth="md">
         <br />
         <Header />
         <br />
-        
-        <Typography variant="h4" fontWeight={700} paragraph>
-          Home
-        </Typography>
 
-        <TableContainer>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Typography variant="h4" fontWeight={700} paragraph>
+            Home
+          </Typography>
+          <div>
+            <Button
+              color="success"
+              variant="contained"
+              onClick={onAdd}
+              startIcon={<Add />}
+            >
+              Add Joke
+            </Button>
+          </div>
+        </Stack>
+
+        <MUIDataTable 
+          title={''}
+          columns={columns}
+          data={jokes}
+          options={options}
+        />
+
+        {/* <TableContainer>
           <Table sx={{}}>
             <TableHead>
               <TableRow>
-                <TableCell>Title</TableCell>
-                <TableCell align="right">Author</TableCell>
-                <TableCell align="right">Created Date</TableCell>
-                <TableCell align="right">Views</TableCell>
+                <TableCell sx={{ fontWeight: 700, fontSize: 18 }}>
+                  Title
+                </TableCell>
+                <TableCell
+                  align="center"
+                  sx={{ fontWeight: 700, fontSize: 18 }}
+                >
+                  Author
+                </TableCell>
+                <TableCell
+                  align="center"
+                  sx={{ fontWeight: 700, fontSize: 18 }}
+                  onClick={() => handleSort("createdAt")}
+                >
+                  Created Date
+                </TableCell>
+                <TableCell
+                  align="center"
+                  sx={{ fontWeight: 700, fontSize: 18 }}
+                  onClick={() => handleSort("views")}
+                >
+                  Views
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {(rowsPerPage > 0
-                ? jokes.slice(
+                ? sortedJokes.slice(
                     page * rowsPerPage,
                     page * rowsPerPage + rowsPerPage
                   )
-                : jokes
+                : sortedJokes
               ).map((joke: Joke) => (
                 <TableRow key={joke.id}>
-                  <TableCell>{joke.title || "-"}</TableCell>
-                  <TableCell align="right">{joke.author || "-"}</TableCell>
-                  <TableCell align="right">{joke.createdAt || "-"}</TableCell>
-                  <TableCell align="right">{joke.views || "-"}</TableCell>
+                  <TableCell sx={{ borderRight: "1px solid" }}>
+                    <Link component={RouterLink} to={`/joke/${joke.id}/edit`}>
+                      {joke.title || "-"}
+                    </Link>
+                  </TableCell>
+                  <TableCell align="center" sx={{ borderRight: "1px solid" }}>
+                    {formatAuthor(joke?.author)}
+                  </TableCell>
+                  <TableCell align="center" sx={{ borderRight: "1px solid" }}>
+                    {formatDate(joke?.createdAt)}
+                  </TableCell>
+                  <TableCell
+                    align="center"
+                    style={{ color: getViewColor(joke?.views) }}
+                  >
+                    {joke.views || "-"}
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
         <TablePagination
-            component='div'
-            rowsPerPageOptions={[5,10]}
-            count={jokes.length}
-            rowsPerPage={rowsPerPage} 
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-        />
+          component="div"
+          rowsPerPageOptions={[5, 10]}
+          count={jokes.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        /> */}
       </Container>
     </>
   );
